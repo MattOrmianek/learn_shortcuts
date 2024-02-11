2ba2a7b72bec6dcd39564eca6762f80a752139a2
Mateusz Ormianek <mattormianek@gmail.com>
2024-02-11 11:50:53 +0100

    feature: added better descriptions what to do

--- create_db.py
+++ create_db.py
@@ -1,3 +1,16 @@
 
 # TODO: create a structured database from the input data
+# Structure of shortcuts table:
+# - id - actual shortcut - description - date_of_creation - date_of_last_usage - usage_count
+
+# Structure of games table:
+# - id - points - ids of shortcuts - mode - rounds - date - time
+
 # TODO: add to a structured database new data
+
+# TODO: choose db to use
+# TODO: create tables
+# TODO: create script for creating db from 0, for test  - create_db_from_zero.py
+# TODO: create connecting to db
+# TODO: create script for adding data to db - add_data_to_db.py
+# TODO: create CRUD operations for db
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
 
-# TODO: importing shortcuts "db"
 # TODO: analyzing the data
-# TODO: waiting for the user to input the data - how many rounds, what mode
+# Read the structured database - how many shortcuts, how many games, how many rounds, how many modes
+# Create recomendations for users - based on the data
+
+# TODO: settings
+# Let user to use basic settings or to edit them
+# shortcuts - from 1 to 100 but counting them from less used to most used
 # rounds - from 1 to 20
 # mode - 1 or 2 or 3
+
+# TODO: gameplay
+# Let user to choose the mode and rounds
+# Let user add new shortcuts - proritize the less used shortcuts
 
